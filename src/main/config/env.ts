a0067918c6ff6d80a65435437c96ba90cc3ec14c
Renato Hideki Osaka <renato.osaka@hotmail.com>
2021-03-05 10:34:03 -0300

    feat: build and docker compose file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,4 @@
 export default {
-  MONGO_URL:
-    process.env.MONGO_URL || 'mongodb://localhost:27017/delivery-much',
+  MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/delivery-much',
   HTTP_PORT: process.env.PORT ?? 5000,
 };
